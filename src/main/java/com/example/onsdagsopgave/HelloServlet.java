109dcc0c0b9823745781c6fff3469a7430f5e53b
Snoopsie1 <oliver.rasoli@gmail.com>
2021-10-27 13:51:16 +0200

    made user form

--- src/main/java/com/example/onsdagsopgave/HelloServlet.java
+++ src/main/java/com/example/onsdagsopgave/HelloServlet.java
@@ -9,16 +9,19 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello World!";
+        message = "Eyyyyyyy Welcome ";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        String navn = request.getParameter("name");
+
         response.setContentType("text/html");
 
         // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
+        out.println("<h1>" + message + navn + "</h1>");
         out.println("</body></html>");
     }
 
--- target/OnsdagsOpgave-1.0-SNAPSHOT/index.jsp
+++ target/OnsdagsOpgave-1.0-SNAPSHOT/index.jsp
@@ -8,6 +8,21 @@
 <h1><%= "Hello World!" %>
 </h1>
 <br/>
+
+<form action="hello-servlet" , method="get">
+    <label for="name">Username:</label><br>
+    <input type="text" id="name" name="name" value="Username">
+    <br>
+    <label for="pass1">Password:</label><br>
+    <input type="text" id="pass1" name="pass1" value="Password">
+    <br>
+    <label for="pass2">Re-type Password:</label><br>
+    <input type="text" id="pass2" name="pass2" value="Re-type Password">
+        <br>
+        <br>
+    <input type="submit" value="Submit">
+</form>
+
 <a href="hello-servlet">Hello Servlet</a>
 </body>
 </html>
